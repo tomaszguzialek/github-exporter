a0d82810af51b3cdceae848d7b8a898825d97de5
Tomasz Guzialek <tgu@trustpilot.com>
2020-05-14 19:10:55 +0200

    Set cookie header for authentication.

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.pdf({path: 'page.pdf'});
+    
+    page.setExtraHTTPHeaders({'Cookie': '<cookie_header_value_placeholder>'});
+    await page.goto('<page_to_be_saved_url_placeholder>');
+    await page.pdf({path: '<pdf_output_file_placeholder>'});
 
     await browser.close();
 })();
