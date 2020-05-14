d672003c1e4431d86c5fb8a2bdbd0f554d135b73
Tomasz Guzialek <tgu@trustpilot.com>
2020-05-14 20:53:18 +0200

    Define saving as PDF as a CLI command with arguments.

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
+const yargs = require('yargs');
+const fs = require('fs').promises;
 const puppeteer = require('puppeteer');
 
-(async () => {
+
+async function readJsonFile(path) {
+    const binaryData = await fs.readFile(path, "binary");
+    return JSON.parse(binaryData);
+}
+
+async function savePageAsPdf(url, outputFilename, headers = undefined) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     
-    page.setExtraHTTPHeaders({'Cookie': '<cookie_header_value_placeholder>'});
-    await page.goto('<page_to_be_saved_url_placeholder>');
-    await page.pdf({path: '<pdf_output_file_placeholder>'});
+    if (headers) {
+        page.setExtraHTTPHeaders(headers);
+    }
+
+    await page.goto(url);
+    await page.pdf({path: outputFilename});
 
     await browser.close();
-})();
+}
+
+async function main() {
+    const argv = yargs
+        .command({
+            command: 'save-pdf [output] [headers-json-file] <url>',
+            desc: 'Save a PDF file from a page',
+            builder: (yargs) => yargs.default('output', 'output.pdf').default('headers-json-file', undefined),
+            handler: async (argv) => {
+                let headers = undefined;
+                if (argv.headersJsonFile) {
+                    headers = await readJsonFile(argv.headersJsonFile);
+                }
+                
+                await savePageAsPdf(argv.url, argv.output, headers);
+            }
+        }) 
+        .help()
+        .alias('help', 'h')
+        .argv;
+};
+
+main();
